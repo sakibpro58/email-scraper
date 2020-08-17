2d8fbc990b9cc63743da74276fdb0fcc1422e8b7
Ariful Alam <swazan.arif@gmail.com>
2020-08-18 02:43:05 +0600

    prod build

---
+++ client/build/precache-manifest.999c8f65ea7cbaa8423b267da30f530e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34e46bafa0e1ed2fb3d4a4c5fa178c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85c3facfe40490fea400",
+    "url": "/static/css/2.aa7e5f6d.chunk.css"
+  },
+  {
+    "revision": "13c01e12833e263b80c2",
+    "url": "/static/css/main.7d30c71a.chunk.css"
+  },
+  {
+    "revision": "85c3facfe40490fea400",
+    "url": "/static/js/2.36a3bf6c.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/static/js/2.36a3bf6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c01e12833e263b80c2",
+    "url": "/static/js/main.4b139c62.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
