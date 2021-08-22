a597cdb5ad2645037a1737eba0f00f249ec6ae7e
sakibpro58 <83116208+sakibpro58@users.noreply.github.com>
2021-08-22 08:47:32 +0600

    Update Navbar.js

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -8,29 +8,13 @@ const Navbar = () => {
             <nav className="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
                 <div className="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
                     <Link className="navbar-brand brand-logo" to="/">
-                        <h3>Email scraper</h3>
+                        <h3>AB Email scraper</h3>
                     </Link>
                     <Link className="navbar-brand" to="/">
-                        <h3 className="px-3 my-auto">Mr. Scraper</h3>
+                        <h3 className="px-3 my-auto">AB Email Scraper</h3>
                     </Link>
                 </div>
-                <div className="navbar-menu-wrapper d-flex align-items-stretch">
-                    <ul className="navbar-nav navbar-nav-right">
-
-                        <li className="nav-item full-screen-link">
-                            {/* eslint-disable-next-line */}
-                            <a className="nav-link">
-                                <i className="mdi mdi-fullscreen" id="fullscreen-button"></i>
-                            </a>
-                        </li>
-                        <li className="nav-item ">
-                            {/* eslint-disable-next-line */}
-                            <a className="nav-link" href="#">
-                                <i className="mdi mdi-format-line-spacing"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                
             </nav>
         </React.Fragment>
     );
