0d9f8d9f39010ffdb7259789b5a4a0726a401786
sakibpro58 <83116208+sakibpro58@users.noreply.github.com>
2021-09-10 10:35:05 +0600

    Update OptionPopup.js

--- client/src/components/OptionPopup.js
+++ client/src/components/OptionPopup.js
@@ -31,31 +31,7 @@ const OptionPopup = (props) => {
                                 </div>
                             </div>
                         </div>
-                        <div className="card mt-2">
-                            <div className="mx-0 row">
-                                <div className="col-md-12">
-                                    <div className="form-group" style={{ marginBottom: '1rem' }}>
-                                        <div className="form-check">
-                                            <label class="form-check-label">
-                                                <input type="radio" class="form-check-input" checked={props.options.searchStrength === 'deep'} name="searchStrength" value="deep" onClick={(e) => { 
-                                                    props.setOptions({...props.options, searchStrength: e.target.value });
-                                                    localStorage.setItem("searchStrength", "deep");
-                                                }}/> Deep Search <i class="input-helper"></i>
-                                            </label>
-                                        </div>
-                                        <div className="form-check">
-                                            <label class="form-check-label">
-                                                <input type="radio" class="form-check-input" checked={props.options.searchStrength === 'quick'} name="searchStrength" value="quick" onClick={(e) => { 
-                                                    props.setOptions({...props.options, searchStrength: e.target.value });
-                                                    localStorage.setItem("searchStrength", "quick");
-                                                }}/> Quick Search <i class="input-helper"></i>
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                        
                 </Modal.Body>
             </Modal>
         </React.Fragment>
